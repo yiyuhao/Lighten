1ce3ee4f8e7e0015fefa07b8a895eb5a5ff8eff6
_GL0oMY <uestcyyh@sina.com>
2017-09-04 20:05:48 +0800

    初始化users app

---
+++ users/models.py
@@ -0,0 +1,21 @@
+# encoding:utf-8
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserProfile(AbstractUser):
+    nickname = models.CharField(max_length=50, verbose_name=u'昵称', default='')
+    birthday = models.DateField(verbose_name=u'生日', null=True, blank=True)
+    gender = models.CharField(max_length=5, choices=(('male', u'男'), ('female', u'女')))
+    address = models.CharField(max_length=100, default=u'')
+    mobile = models.CharField(max_length=11, null=True, blank=True)
+    image = models.ImageField(upload_to='image/%Y/%m', default='image/default.png', max_length=100)
+
+    class Meta:
+        verbose_name = '用户信息'
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.username
