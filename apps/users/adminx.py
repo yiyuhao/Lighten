a2962c888bebc8539b2431bbc9bd570364ed6f23
_GL0oMY <uestcyyh@sina.com>
2017-09-06 15:51:13 +0800

    xadmin全局配置：主题、site_title、site_footer、menu_style: accordion

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,7 +1,19 @@
 # coding: utf-8
 import xadmin
+from xadmin import views
 
 from .models import UserProfile, EmailVerifyRecord, Banner
+
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSetting(object):
+    site_title = "Lighten后台管理"
+    site_footer = 'Lighten'
+    menu_style = 'accordion'
 
 
 # class UserProfileAdmin(object):
@@ -29,3 +41,5 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSetting)
