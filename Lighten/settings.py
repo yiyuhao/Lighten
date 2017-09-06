a2a1f8b854b2ec902a63b3927391ce68cc2f8010
_GL0oMY <uestcyyh@sina.com>
2017-09-06 10:40:37 +0800

    xadmin git源码安装、配置

--- Lighten/settings.py
+++ Lighten/settings.py
@@ -15,7 +15,8 @@ import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+sys.path.append(os.path.join(BASE_DIR, 'apps'))
+sys.path.append(os.path.join(BASE_DIR, 'extra_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
