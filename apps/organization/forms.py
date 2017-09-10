2c96111128778799447fdb8a3e87ce633a5a0ac1
_GL0oMY <uestcyyh@sina.com>
2017-09-10 12:12:26 +0800

    新增: UserAskForm对手机号的正则验证

--- apps/organization/forms.py
+++ apps/organization/forms.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+import re
+
 from django import forms
 
 from operation.models import UserAsk
@@ -10,3 +12,15 @@ class UserAskForm(forms.ModelForm):
     class Meta:
         model = UserAsk
         fields = ['name', 'mobile', 'course_name']
+
+    def clean_mobile(self):
+        """
+        验证手机号码是否合法
+        :return: None
+        """
+        mobile = self.cleaned_data['mobile']
+        REGEX_MOBILE = '^1[358]\d{9}$|^147\d{8}$|^176\d{8}$'
+        if re.match(REGEX_MOBILE, mobile):
+            return mobile
+        else:
+            raise forms.ValidationError(u'手机号码非法', code='mobile_invalid')
