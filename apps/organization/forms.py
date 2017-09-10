b773dcc9a19f39c8a6fea998a4a8130dfc5f9f78
_GL0oMY <uestcyyh@sina.com>
2017-09-10 10:11:25 +0800

    新增: UserAskForm(forms.ModelForm)

---
+++ apps/organization/forms.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from django import forms
+
+from operation.models import UserAsk
+
+
+class UserAskForm(forms.ModelForm):
+
+    class Meta:
+        model = UserAsk
+        fields = ['name', 'mobile', 'course_name']
