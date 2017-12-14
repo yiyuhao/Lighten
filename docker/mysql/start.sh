1d2886b54a82a0856d69b5ca0af4ef1eadf9a707
_GL0oMY <uestcyyh@sina.com>
2017-12-14 12:21:42 +0800

    修改: Dockerfile

--- docker/mysql/start.sh
+++ docker/mysql/start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash 
 
+# 镜像存在时，检查容器是否存在
+if docker ps -a | grep -i mysql; then
+    # 容器存在时则删除容器
+    echo 'The docker container <mysql> already exist, deleting it...'
+    docker rm -f mysql
+fi
+
 echo "create a mysql container.."
 docker run -d --name mysql \
            -v $(pwd)/conf.d:/etc/mysql/conf.d \
